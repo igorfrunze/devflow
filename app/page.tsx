f248a20707db553014231793ecabcd6a2d920f55
igor_frunze <igor_frunze@live.com>
2025-08-25 12:19:04 +0300

    Eslint & Prettier

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <h1 className="text-3xl text-violet-700 font-black">
+        Welcome to Next.js 👋
+      </h1>
+    </div>
+  );
 };
 
 export default Home;
