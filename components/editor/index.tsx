203e8e5778d78b09332ea7bf3331e5129bfc89b1
igor_frunze <igor_frunze@live.com>
2025-09-19 14:58:35 +0300

    implement Tag input

--- components/editor/index.tsx
+++ components/editor/index.tsx
@@ -50,7 +50,7 @@ const Editor = ({ value, fieldChange, editorRef, ...props }: Props) => {
       ref={editorRef}
       markdown={value}
       onChange={fieldChange}
-      className="background-light800_dark200 light-border-2 markdown-editor dark-editor w-full border"
+      className="background-light800_dark200 light-border-2 markdown-editor dark-editor w-full border grid"
       plugins={[
         headingsPlugin(),
         listsPlugin(),
